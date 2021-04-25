3c493740449da69bdb13cb7add24766b3410a97f
vandanachandola <vandana.chandola@gmail.com>
2021-04-25 09:10:40 -0700

    update deployment changes

--- backend/config.js
+++ backend/config.js
@@ -20,7 +20,7 @@ config.awss3.AWS_ACL_ACCESS_CONTROL = 'public-read';
 
 config.kafka = {};
 config.kafka.port = 5001;
-config.kafka.host = 'localhost';
+config.kafka.host = 'http://52.14.30.76';
 config.kafka.serverport = 2181;
 config.kafka.url = `${config.kafka.host}:${config.kafka.serverport}`;
 
