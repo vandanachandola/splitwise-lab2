638ec5c81dee716ac27816f9f33f2ad43817a236
Vandana Chandola <vandana.chandola@gmail.com>
2021-10-15 12:15:58 -0700

    config related changes

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,13 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const config = {};
 
 config.db = {};
-config.db.username = 'admin';
-config.db.password = 'splitwise';
-config.db.dbname = 'splitwise_db';
+config.db.username = process.env.DB_USER;
+config.db.password = process.env.DB_PWD;
+config.db.dbname = process.env.DB_NAME;
 config.db.conn = `mongodb+srv://${config.db.username}:${config.db.password}@cluster0.1arhg.mongodb.net/${config.db.dbname}?retryWrites=true&w=majority`;
 config.server = {};
 config.server.port = process.env.PORT || 5000;
@@ -12,16 +16,16 @@ config.auth = {};
 config.auth.secretOrKey = 'secret';
 
 config.awss3 = {};
-config.awss3.AWS_ACCESS_KEY_ID = 'AKIAQ5M2SXSO3GK54E7H';
-config.awss3.AWS_SECRET_ACCESS_KEY = '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';
-config.awss3.AWS_S3_BUCKET_NAME = 'splitwise-lab2-bucket';
-config.awss3.AWS_LOCATION = 'us-east-2';
-config.awss3.AWS_ACL_ACCESS_CONTROL = 'public-read';
+config.awss3.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+config.awss3.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+config.awss3.AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+config.awss3.AWS_LOCATION = process.env.AWS_LOCATION;
+config.awss3.AWS_ACL_ACCESS_CONTROL = process.env.AWS_ACL_ACCESS_CONTROL;
 
 config.kafka = {};
-config.kafka.port = 5001;
-config.kafka.host = 'http://18.223.2.203';
-config.kafka.serverport = 2181;
+config.kafka.port = process.env.KAFKA_PORT;
+config.kafka.host = process.env.KAFKA_HOST;
+config.kafka.serverport = process.env.KAFKA_SERVER_PORT;
 config.kafka.url = `${config.kafka.host}:${config.kafka.serverport}`;
 
 module.exports = config;
--- frontend/src/shared/config.js
+++ frontend/src/shared/config.js
@@ -1,7 +1,7 @@
 const config = {};
 
 config.server = {};
-config.server.host = 'http://18.223.2.203';
+config.server.host = 'http://localhost';
 config.server.port = '5000';
 config.server.url = `${config.server.host}:${config.server.port}`;
 
