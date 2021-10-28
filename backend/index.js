e3a9db06d505d149b293a497778e51540251b677
Vandana Chandola <vandana.chandola@gmail.com>
2021-10-28 02:26:35 -0700

    port config updated

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use('/api/users', userRoute);
 app.use('/api/groups', groupRoute);
 app.use('/api/transactions', transactionRoute);
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGODB_URI || config.db.conn, {
     useNewUrlParser: true,
