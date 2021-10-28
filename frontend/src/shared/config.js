59dd84fe56547dfcd6d6e0d0e8e9cf4f54e99d14
Vandana Chandola <vandana.chandola@gmail.com>
2021-10-28 11:45:28 -0700

    frontend config update

--- frontend/src/shared/config.js
+++ frontend/src/shared/config.js
@@ -2,8 +2,7 @@ const config = {};
 
 config.server = {};
 config.server.host = process.env.BACKEND_URL || 'http://localhost';
-config.server.port = '5000';
-config.server.url =
-  process.env.PORT || `${config.server.host}:${config.server.port}`;
+config.server.port = process.env.PORT || '5000';
+config.server.url = `${config.server.host}:${config.server.port}`;
 
 module.exports = config;
