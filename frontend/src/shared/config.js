6edcada52d231aa78edbec71aeb8b79f7c06b331
Vandana Chandola <vandana.chandola@gmail.com>
2021-10-28 02:54:24 -0700

    frontend env variables set

--- frontend/src/shared/config.js
+++ frontend/src/shared/config.js
@@ -1,8 +1,9 @@
 const config = {};
 
 config.server = {};
-config.server.host = 'http://localhost';
+config.server.host = process.env.BACKEND_URL || 'http://localhost';
 config.server.port = '5000';
-config.server.url = `${config.server.host}:${config.server.port}`;
+config.server.url =
+  process.env.PORT || `${config.server.host}:${config.server.port}`;
 
 module.exports = config;
